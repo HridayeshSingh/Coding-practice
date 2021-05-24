849e410d404a23c55fc96d2bb0a7213c6de54375
HridayeshSingh <50636882+HridayeshSingh@users.noreply.github.com>
2021-05-24 15:52:40 +0530

    Create Set Matrix Zeros - Leetcode.cpp

---
+++ Arrays/Medium/Set Matrix Zeros - Leetcode.cpp
@@ -0,0 +1,34 @@
+// -> https://leetcode.com/problems/set-matrix-zeroes/
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        bool isFirstColZero = false;
+        
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0)
+                isFirstColZero = true;
+            for(int j = 1; j < n; j++)
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+        
+        if(matrix[0][0] == 0)
+            for(int j = 1; j < n; j++)
+                matrix[0][j] = 0;
+        
+        if(isFirstColZero)
+            for(int i = 0; i < m; i++)
+                matrix[i][0] = 0;
+    }
+};
